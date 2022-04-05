88dd37e12b9d39ea2c45dadff96e4b3878b3fabf
Rodolfo Olivarria <olivarria.rodolfo@gmail.com>
2022-04-04 20:04:17 -0700

    fixed feedback list

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -32,22 +32,22 @@ function FeedbackList() {
     </div>
   );
 
-  return (
-    <div className="feedback-list">
-      <AnimatePresence>
-        {feedback.map((item) => (
-          <motion.div
-            key={item.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <FeedbackItem key={item.id} item={item} />
-          </motion.div>
-        ))}
-      </AnimatePresence>
-    </div>
-  );
+  // return (
+  //   <div className="feedback-list">
+  //     <AnimatePresence>
+  //       {feedback.map((item) => (
+  //         <motion.div
+  //           key={item.id}
+  //           initial={{ opacity: 0 }}
+  //           animate={{ opacity: 1 }}
+  //           exit={{ opacity: 0 }}
+  //         >
+  //           <FeedbackItem key={item.id} item={item} />
+  //         </motion.div>
+  //       ))}
+  //     </AnimatePresence>
+  //   </div>
+  // );
 }
 
 export default FeedbackList;
